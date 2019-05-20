d0ab6a00ab48acf76818f24c9e2f6dc544ce06ec
ma-daria <marina.dasha18@yandex.ru>
2019-05-20 20:00:14 +0700

    1я страница

--- analysis/views.py
+++ analysis/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h3>hello!</h3>")
+    return render(request, 'analysis/index.html')
