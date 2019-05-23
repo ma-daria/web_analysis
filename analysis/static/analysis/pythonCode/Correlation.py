3c092f00aa3850fd548f946e92b3ef8a7d75de67
ma-daria <marina.dasha18@yandex.ru>
2019-05-24 00:52:31 +0700

    update

--- analysis/static/analysis/pythonCode/Correlation.py
+++ analysis/static/analysis/pythonCode/Correlation.py
@@ -5,6 +5,7 @@ def CreateСorrelationСhemistry(measurement):
     mapPalette = Include.sns.diverging_palette(10, 240, sep=10, as_cmap=True)
     Include.plt.figure(figsize=(15, 15), dpi=200)
     сorrelationСhemistry = measurement.corr()
+    print(сorrelationСhemistry)
     Include.sns.heatmap(сorrelationСhemistry, cmap=mapPalette, vmin=-1, vmax=1, annot=True)
     Include.plt.savefig(str(settings.STATIC_ROOT_A) + "/analysis/image/СorrelationСhemistry.png")
     # Include.plt.savefig("/home/ma-daria/PycharmProjects/web_analysis/analysis/static/analysis/image/СorrelationСhemistry.png")
