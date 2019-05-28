d36b1a0fb01613da2e14f3dd70c6f4aa83ce4553
ma-daria <marina.dasha18@yandex.ru>
2019-05-28 21:42:17 +0700

    plt.figure(figsize=(15, 15), dpi=200)

--- analysis/static/analysis/pythonCode/CorrelationZooplankton.py
+++ analysis/static/analysis/pythonCode/CorrelationZooplankton.py
@@ -7,7 +7,7 @@ class CorrelationZooplankton(Correlation.Correlation):
 
     def _toDo(self, measurement):
         mapPalette = Include.sns.diverging_palette(10, 240, sep=10, as_cmap=True)
-        Include.plt.figure(figsize=(20, 20), dpi=200)
+        Include.plt.figure(figsize=(15, 15), dpi=200)
         correlationZooplankton = measurement.corr()
         Include.sns.heatmap(correlationZooplankton, cmap=mapPalette, vmin=-1, vmax=1)
         Include.plt.savefig(str(settings.STATIC_ROOT_A) + "/analysis/image/CorrelationZooplankton.png")
--- analysis/static/analysis/pythonCode/Dentogram.py
+++ analysis/static/analysis/pythonCode/Dentogram.py
@@ -15,7 +15,7 @@ class Dentogram(object):
     def _ClusteringMetod(self, measurement, index, col):
         distance_mat = Include.distance.pdist(measurement, 'cosine')
         Z = Include.hierarchy.linkage(distance_mat, 'single')
-        Include.plt.figure(figsize=(20, 10))
+        Include.plt.figure(figsize=(15, 15), dpi=200)
         dn = Include.hierarchy.dendrogram(Z, labels=col, color_threshold=index)
         return Z
 
