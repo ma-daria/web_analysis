432ad0f0136f1324b3f7c2b2eea95e2bb7deba9d
ma-daria <marina.dasha18@yandex.ru>
2019-05-29 12:40:14 +0700

    попытки что-либо исправить с matplotlib

--- analysis/static/analysis/pythonCode/Include.py
+++ analysis/static/analysis/pythonCode/Include.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 from scipy.spatial import distance
 from sklearn import preprocessing
 from scipy.cluster import hierarchy
