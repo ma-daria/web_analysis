f0f256b1c67b4135126f15082c92a9dc9aae930d
ma-daria <marina.dasha18@yandex.ru>
2019-06-04 12:47:12 +0700

    можно выбрать не меньше 2х в селекте

--- analysis/static/analysis/js/select2.js
+++ analysis/static/analysis/js/select2.js
@@ -1,3 +1,29 @@
+disabled();
+function disabled(){
+    but = document.getElementById("but");
+    but.disabled = true;
+}
+
+function undisabled(){
+    but = document.getElementById("but");
+    but.disabled = false;
+}
+
+
+$("#name2").change(function(){
+    var minimum = 2;
+     if($(".js-example-basic-multiple").select2('data').length>=minimum){
+         undisabled();
+     }
+     else {
+         disabled()
+     }
+});
+
+
 $(document).ready(function() {
     $("#name").select2();
 });
+
+
+
