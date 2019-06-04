c116592af3213e522c48f5bd7185b7873dc8205a
ma-daria <marina.dasha18@yandex.ru>
2019-06-04 16:01:38 +0700

    update

--- analysis/static/analysis/pythonCode/Include.py
+++ analysis/static/analysis/pythonCode/Include.py
@@ -1,9 +1,9 @@
+import matplotlib
+matplotlib.use('Agg')
 import pandas as pd
 import numpy as np
 import seaborn as sns
 # import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from scipy.spatial import distance
 from sklearn import preprocessing
