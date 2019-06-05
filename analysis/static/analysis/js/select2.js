52aff1049c4545491e535a9aa120e9c9cfa98c47
ma-daria <marina.dasha18@yandex.ru>
2019-06-05 13:25:40 +0700

    correlation немного подвижек в js

--- analysis/static/analysis/js/select2.js
+++ analysis/static/analysis/js/select2.js
@@ -1,51 +1,10 @@
-disabledSelect();
-
-
-function disabledButton(){
-    but = document.getElementById("but");
-    but.disabled = true;
-}
-
-function undisabledButton(){
-    but = document.getElementById("but");
-    but.disabled = false;
-}
-
-
-function disabledSelect(){
-    but = document.getElementById("name2");
-    but.disabled = true;
-}
-
-function undisabledSelect(){
-    but = document.getElementById("name2");
-    but.disabled = false;
-}
-
-$("#name2").change(function(){
-    var minimum = 2;
-    len = $(".js-example-basic-multiple").select2('data').length;
-     if(len>=minimum){
-         undisabledButton();
-     }else {
-         disabledButton();
-     }
-});
-
-$("#name3").change(function(){
-    text = document.getElementById("name3");
-    if (text.value === 'Корреляция химического и видового состава') {
-        undisabledSelect();
-        disabledButton();
-    }else {
-        disabledSelect();
-    }
-
-});
-
 
 $(document).ready(function() {
     $("#name").select2();
+});
+
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
 });
 
 
